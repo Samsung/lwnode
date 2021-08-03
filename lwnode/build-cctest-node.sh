a58a35776c19b4db2c0c9609dd56f990a756fdcb
Daeyeon Jeong <daeyeon.jeong@samsung.com>
2021-08-03 15:37:37 +0900

    test: add node cctest
    Signed-off-by: Daeyeon Jeong daeyeon.jeong@samsung.com

---
+++ lwnode/build-cctest-node.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright (c) 2021-present Samsung Electronics Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+CONFIG="--without-npm --without-bundled-v8 \
+    --without-inspector --without-node-code-cache --without-node-snapshot \
+    --with-intl none --shared-openssl --shared-zlib \
+    --dest-os linux --dest-cpu x64 \
+    --engine escargot \
+    --ninja"
+
+if [[ $1 =~ ^"-d" ]]; then
+  ./configure $CONFIG --debug --debug-node
+  ninja -v -C out/Debug cctest |& lwnode/tools/colorize.sh
+
+else
+  ./configure $CONFIG
+  ninja -v -C out/Release cctest |& lwnode/tools/colorize.sh
+fi
