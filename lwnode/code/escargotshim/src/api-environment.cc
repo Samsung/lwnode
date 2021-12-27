ca13725c4c152ed6513dd412ccc273e9c0801e19
Ryan Hyun Choi <ryan.choi@samsung.com>
2021-12-27 14:32:54 +0900

    feat: Add v8 Extension framework
    Signed-off-by: Ryan Choi <ryan.choi@samsung.com>

--- lwnode/code/escargotshim/src/api-environment.cc
+++ lwnode/code/escargotshim/src/api-environment.cc
@@ -39,6 +39,7 @@ void v8::V8::ShutdownPlatform() {
 
 bool v8::V8::Initialize(const int build_config) {
   Engine::Initialize();
+  RegisterExtension(std::make_unique<ExternalizeStringExtension>());
   return true;
 }
 
