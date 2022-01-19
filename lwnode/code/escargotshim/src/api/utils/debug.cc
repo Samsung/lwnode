63159713c56cfcbd7cd8432990a2593da95776d6
Hosung Kim <hs852.kim@samsung.com>
2022-01-19 14:16:33 +0900

    feat: add some debug util
     - print object of Escargot value
     - print tostring of value
     - convert local value of v8 into standard string
    
    Signed-off-by: Hosung Kim hs852.kim@samsung.com

--- lwnode/code/escargotshim/src/api/utils/debug.cc
+++ lwnode/code/escargotshim/src/api/utils/debug.cc
@@ -18,8 +18,15 @@
 #if defined(LWNODE_PLATFORM_LINUX)
 #include <execinfo.h>
 #endif
+
+#include "api/context.h"
+#include "api/es-helper.h"
+#include "api/isolate.h"
 #include "debug.h"
 #include "misc.h"
+
+using namespace Escargot;
+using namespace v8;
 
 namespace EscargotShim {
 
@@ -95,6 +102,69 @@ void DebugUtils::printStackTrace() {
     }
   }
 #endif
+}
+
+static void printObjectProperties(ExecutionStateRef* state,
+                                  ObjectRef* object,
+                                  int depth = 0) {
+  auto propertyNames = object->ownPropertyKeys(state);
+  LWNODE_LOG_RAW("%*s[prop(%zu)]\n", depth, "", propertyNames->size());
+  for (size_t i = 0; i < propertyNames->size(); i++) {
+    auto propertyValue = object->getOwnProperty(state, propertyNames->at(i));
+    auto propertyNameString = propertyNames->at(i)->toString(state);
+    auto propertyValueString = propertyValue->toString(state);
+    LWNODE_LOG_RAW("  %*s%s: %s\n",
+                   depth,
+                   "",
+                   propertyNameString->toStdUTF8String().data(),
+                   propertyValueString->toStdUTF8String().data());
+  }
+}
+
+void DebugUtils::printObject(ObjectRef* value, int depth) {
+  auto lwIsolate = IsolateWrap::GetCurrent();
+  auto r = Evaluator::execute(
+      lwIsolate->GetCurrentContext()->get(),
+      [](ExecutionStateRef* state, ObjectRef* object, int depth) -> ValueRef* {
+        printObjectProperties(state, object, depth);
+
+        OptionalRef<ObjectRef> maybePrototype =
+            object->getPrototypeObject(state);
+        if (maybePrototype.hasValue()) {
+          auto prototype = maybePrototype.value();
+          printObjectProperties(state, prototype, depth);
+
+          OptionalRef<ObjectRef> maybePrototype =
+              prototype->getPrototypeObject(state);
+          if (maybePrototype.hasValue()) {
+            printObject(maybePrototype.value(), depth + 2);
+          }
+        }
+        return ValueRef::createNull();
+      },
+      value,
+      depth);
+  LWNODE_CHECK(r.isSuccessful());
+}
+
+void DebugUtils::printToString(ValueRef* value) {
+  auto context = IsolateWrap::GetCurrent()->GetCurrentContext()->get();
+  EvalResult r = Evaluator::execute(
+      context,
+      [](ExecutionStateRef* state, ValueRef* value) -> ValueRef* {
+        auto valueString = value->toString(state);
+        LWNODE_LOG_RAW("toString: %s\n",
+                       valueString->toStdUTF8String().c_str());
+        return value;
+      },
+      value);
+}
+
+std::string DebugUtils::v8StringToStd(Isolate* isolate, Local<Value> value) {
+  Local<String> s;
+  value->ToString(isolate->GetCurrentContext()).ToLocal(&s);
+  v8::String::Utf8Value utf8String(isolate, s);
+  return std::string(*utf8String);
 }
 
 }  // namespace EscargotShim
