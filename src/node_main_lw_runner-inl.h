a11afe5376adf7ea86afd2ad806e67e2cbf7401b
Hosung Kim <hs852.kim@samsung.com>
2022-03-30 19:13:37 +0900

    feat: add flag to enable gmain loop
    Signed-off-by: Hosung Kim hs852.kim@samsung.com

--- src/node_main_lw_runner-inl.h
+++ src/node_main_lw_runner-inl.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2022-present Samsung Electronics Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 #include "node_bindings.h"
 #include "node_internals.h"
@@ -118,8 +134,13 @@ class LWNodeMainRunner {
             node::performance::NODE_PERFORMANCE_MILESTONE_LOOP_START);
 
         // Run main loop
-        std::unique_ptr<MainLoopStrategy> mainLoop =
-            std::make_unique<LoopStrategy>();
+        std::unique_ptr<MainLoopStrategy> mainLoop;
+        if (GmainLoopNodeBindings::isEnabled()) {
+          mainLoop = std::make_unique<GmainLoopStrategy>();
+        } else {
+          mainLoop = std::make_unique<LoopStrategy>();
+        }
+
         mainLoop->RunLoop(env_.get());
 
         env_->performance_state()->Mark(
