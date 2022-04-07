0f0edd38e5f362c60506d00bdbafe9b5f7d3f13e
Daeyeon Jeong <daeyeon.jeong@samsung.com>
2022-04-07 10:36:10 +0900

    feat: emit 'max'
    Signed-off-by: Daeyeon Jeong daeyeon.dev@gmail.com

--- lwnode/test/node/test/memory/test-memory-watch.js
+++ lwnode/test/node/test/memory/test-memory-watch.js
@@ -1,12 +1,30 @@
 const { MemWatcher } = process.lwnode;
 
-const watcher = new MemWatcher();
+let theThing = null;
+let closureLeak = function () {
+  let originalThing = theThing; // leak
+  let unused = function () {
+    if (originalThing) console.log('hi');
+  };
+  theThing = {
+    longStr: new Array(1000000).join('*'), // leak
+    someMethod: function () {},
+  };
+};
 
-watcher.on("stats", (stats) => {
+console.warn('This code intentionally makes leaks');
+const id = setInterval(closureLeak, 1000);
+
+const watcher = new MemWatcher();
+watcher.on('stats', (stats) => {
   console.log(new Date().toTimeString());
   console.table(stats);
 });
+watcher.on('max', (stats) => {
+  console.log(stats);
+});
 
 setTimeout(() => {
+  clearInterval(id);
   watcher.end();
-}, 1000 * 20);
+}, 1000 * 60);
