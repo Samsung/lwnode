523a875d8681e18d1db923ab124e4012c9a809d1
Daeyeon Jeong <daeyeon.jeong@samsung.com>
2022-04-21 11:50:28 +0900

    test: add memory watcher tc
    Signed-off-by: Daeyeon Jeong daeyeon.jeong@samsung.com

---
+++ test/parallel/test-lwnode-memory-watcher.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const common = require('../common');
+const assert = require('assert');
+
+if (!process.lwnode) common.skip("`process.lwnode` doesn't exist");
+
+const { MemWatcher, MemDiff } = process.lwnode;
+
+// diff
+const diff = new MemDiff();
+
+checkDiffProps(diff.check());
+
+function checkDiffProps(r) {
+  for (const v of Object.values(r)) {
+    for (const k of ['heap', 'sinceLastGc', 'unmapped']) {
+      assert.equal(Array.isArray(v[k]), true);
+      assert.equal(v[k].length, 2);
+    }
+  }
+}
+
+// watcher
+const watcher = new MemWatcher();
+
+watcher.on(
+  'stats',
+  common.mustCallAtLeast((stats) => {
+    checkDiffProps(stats);
+  }, 1),
+);
+
+watcher.on(
+  'max',
+  common.mustCall((stats) => {
+    teardown();
+  }),
+);
+
+const idi = setInterval(makesLeaks(), 500);
+const idt = setTimeout(teardown, common.platformTimeout(1000 * 60));
+
+function teardown() {
+  watcher.end();
+  clearInterval(idi);
+  clearTimeout(idt);
+}
+
+function makesLeaks() {
+  let theThing = null;
+  let closureLeak = function () {
+    let originalThing = theThing; // leak
+    let unused = function () {
+      if (originalThing) console.log('hi');
+    };
+    theThing = {
+      longStr: new Array(1000000).join('*'), // leak
+      someMethod: function () {},
+    };
+  };
+  return closureLeak;
+}
