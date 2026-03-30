3653b252fe86cbd0bb65d18e6e743d6bfc8791ab
Hosung Kim <hs852.kim@samsung.com>
2026-03-30 14:20:36 +0900

    feat: add default timeout in ClientRequest
    Signed-off-by: Hosung Kim hs852.kim@samsung.com

--- deps/node/test/parallel/test-http-client-set-timeout-after-end.js
+++ deps/node/test/parallel/test-http-client-set-timeout-after-end.js
@@ -20,7 +20,7 @@ server.listen(0, () => {
   const req = get({ agent, port }, (res) => {
     res.on('end', () => {
       strictEqual(req.setTimeout(0), req);
-      strictEqual(socket.listenerCount('timeout'), 1);
+      strictEqual(socket.listenerCount('timeout'), 2); // @lwnode(1 -> 2): exist default timeout
       agent.destroy();
       server.close();
     });
